730e320a4b3b22c44ad42fb761116800452b64bd
Daniel <danieel47896321@gmail.com>
2022-12-21 09:57:36 +0200

    made opening and play background. added green color. added translate. finish MainActivity xml and kotlin. created play page.

--- app/src/main/java/com/example/softwarequiztime/MainActivity.kt
+++ app/src/main/java/com/example/softwarequiztime/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.softwarequiztime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var playButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init(){
+        playButton = findViewById(R.id.playButton)
+        setPlayButton()
+    }
+    private fun setPlayButton(){
+        playButton.setOnClickListener{
+            startActivity(Intent(this,Play::class.java))
+            finish()
+        }
     }
 }
